c1ce514ec023b3abf2d2f4863413475800bf9487
Cristian <cristianbolanos@yahoo.com>
2024-02-29 14:17:13 -0500

    fixed hosting

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -29,10 +29,10 @@ const Home: React.FC = () => {
         <h1 className="welcome-title">Welcome to the Vision AI App</h1>
 
         <p className="app-description">
-          A Artificial Intelligence Based Near Vision Tester. <br /> Providing
+          An Artificial Intelligence Based Near Vision Tester. <br /> Providing
           accurate and reliable at home vision testing.
         </p>
-        <Button buttonText="Continue" onClickAction={goToTermsPage} />
+        <Button buttonText="Get Started" onClickAction={goToTermsPage} />
       </IonContent>
       <IonFooter>
         <IonToolbar>
